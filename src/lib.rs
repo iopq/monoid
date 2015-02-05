feef6ed022e607788cbb4e78955f1345c2311eb5
Igor Polyakov <iopguy@gmail.com>
2015-02-05 06:03:08 -0800

    adding cowstring to monoids

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+extern crate test;
+
+use std::string::CowString;
+use std::borrow::IntoCow;
+
 pub trait Monoid {
     // don't have associated values yet, so use a nullary function
     fn id() -> Self;
@@ -5,6 +10,15 @@ pub trait Monoid {
     // this version consumes arguments
     // a non-consuming version might be possible
     fn op(self, other: Self) -> Self;
+}
+
+impl Monoid for CowString<'static> {
+    fn id() -> CowString<'static> { "".into_cow() }
+    fn op(self, other: CowString<'static>) -> CowString<'static> {
+        let mut owned = self.into_owned();
+        owned.push_str(&*other);
+        owned.into_cow()
+    }
 }
 
 // Strings are a Monoid over concatenation
@@ -30,10 +44,16 @@ impl<A: Monoid> Monoid for Option<A> {
 #[cfg(test)]
 mod tests {
     use super::Monoid;
+    use std::string::CowString;
+    use std::borrow::IntoCow;
     
     #[test]
     fn string_append() {
         assert_eq!("abcdef".to_string(), "abc".to_string().op("def".to_string()));
+    }
+    
+    fn cowstring_append() {
+        assert_eq!("abcdef".into_cow(), "abc".into_cow().op("def".into_cow()));
     }
     
     #[test]
@@ -54,4 +74,24 @@ mod tests {
     fn both() {
         assert_eq!(Some("oneone".to_string()), Some("one".to_string()).op(Some("one".to_string())));
     }
+}
+
+#[bench]
+fn bench_cowstring(b: &mut test::Bencher) {
+    b.iter(|| {
+        let mut fizz = "Fizz".into_cow();
+        let mut buzz = "Buzz".into_cow();
+        (fizz.into_owned() + &*buzz).into_cow()
+    });
+}
+
+#[bench]
+fn bench_cowstring_ms2ger(b: &mut test::Bencher) {
+    b.iter( || {  
+            let mut fizz = "Fizz".into_cow().into_owned();
+            let mut buzz = "Buzz".into_cow();
+            fizz.push_str(&*buzz);
+            fizz.into_cow();
+        }
+    );
 }
